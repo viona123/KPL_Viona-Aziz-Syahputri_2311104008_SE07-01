4c63a56904c75d1a126fdd79aaf67e6b717494ce
viona123 <syahputriviona@gmail.com>
2025-02-24 12:20:45 +0700

    first commit

---
+++ 02_Pengenalan_IDE_dan_Permrograman_NodeJS/functions.js
@@ -0,0 +1,24 @@
+// named function
+function perkalian(a, b) {
+    let result = a*b
+    return result
+}
+
+// expresif function 
+// arrow function
+// anonymous function
+const pertambahan = (a, b) => {
+    let result = a+b
+    return result
+}
+
+// named expression function
+const pembagian = function bagi(a,b) {
+    let result = a/b
+    return result
+}
+
+// named arroe function
+console.log(perkalian(5,2))
+console.log(pertambahan(6,2))
+console.log(pembagian(8,2))
---
+++ 02_Pengenalan_IDE_dan_Permrograman_NodeJS/variables.js
@@ -0,0 +1,19 @@
+var name = "Viona Aziz Syahputri"
+const nim = 2311104008
+
+console.log(name)
+
+console.log("nama saya adalah" + name + "dan nim saya adalah" + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+
+
+    var name = "ayam"
+
+    let fullName = "Viona Aziz Syahputri"
+
+    nim = 2311104008;
