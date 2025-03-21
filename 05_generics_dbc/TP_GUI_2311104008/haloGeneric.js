cbfb63eb50901aa3ca32c6628697b92454375589
viona123 <syahputriviona@gmail.com>
2025-03-21 09:58:59 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_generics_dbc/TP_GUI_2311104008/haloGeneric.js
@@ -0,0 +1,11 @@
+class HaloGeneric {
+    sapaUser(user) {
+        console.log(`Halo user ${user}`);
+    }
+}
+
+// Contoh penggunaan
+const halo = new HaloGeneric();
+halo.sapaUser("Viona Aziz Syahputri");
+
+module.exports = HaloGeneric;
