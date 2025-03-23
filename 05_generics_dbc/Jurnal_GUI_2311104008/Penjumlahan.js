93c85459daa0e9a27fbd047896afb05a8e539ddf
viona123 <syahputriviona@gmail.com>
2025-03-23 16:10:25 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_generics_dbc/Jurnal_GUI_2311104008/Penjumlahan.js
@@ -0,0 +1,8 @@
+class Penjumlahan {
+    static jumlahTigaAngka(a, b, c) {
+        return a + b + c;
+    }
+}
+
+const nimDigits = [23, 11, 10];  
+console.log(Penjumlahan.jumlahTigaAngka(...nimDigits));
