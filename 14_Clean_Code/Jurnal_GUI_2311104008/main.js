237469f4b73b580edd2f016a33e8e386e91cb4dd
viona123 <syahputriviona@gmail.com>
2025-06-05 00:34:03 +0700

    modul 14

---
+++ 14_Clean_Code/Jurnal_GUI_2311104008/main.js
@@ -0,0 +1,8 @@
+// Fungsi untuk menghitung luas persegi panjang
+function hitungLuasPersegiPanjang(panjang, lebar) {
+  const hasil = panjang * lebar;
+  console.log(`Hasil: ${hasil}`);
+}
+
+hitungLuasPersegiPanjang(5, 3);
+
---
+++ 14_Clean_Code/TP_GUI_2311104008/main.js
@@ -0,0 +1,8 @@
+// Menghitung luas persegi berdasarkan sisi
+function hitungLuasPersegi(sisi) {
+  const luas = sisi * sisi;
+  console.log(`Luas: ${luas}`);
+}
+
+hitungLuasPersegi(5);
+
